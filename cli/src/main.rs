28b98ed1d5b81727783c149c9ec5dbbe4a0a69ff
RemasteredArch <81265470+RemasteredArch@users.noreply.github.com>
2024-03-31 16:21:46 -0700

    added support for using a config file

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,13 +1,19 @@
 use clap::Parser;
+use serde::Deserialize;
 use std::path::Path;
 
 fn main() {
-    println!("{}", u16::MAX);
+    let arguments = Arguments::parse();
+    println!("");
 }
 
-#[derive(Parser)]
+#[derive(Parser, Deserialize)]
 #[command(about, version, long_about = None)]
 struct Arguments {
+    #[arg(short = 'f', long = "config-file")]
+    #[serde(skip, default)]
+    config: Option<Box<Path>>,
+
     #[arg(short = '4', long = "IPv4", default_value = "/usr/share/tor/geoip")]
     ipv4: Box<Path>,
 
@@ -19,4 +25,12 @@ struct Arguments {
 
     #[arg(short = 'p', long = "port", default_value = "26000")]
     port: u16,
+}
+
+#[allow(dead_code)]
+fn default_config_path() -> Box<Path> {
+    dirs::config_dir()
+        .unwrap()
+        .join(env!("CARGO_PKG_NAME"))
+        .into_boxed_path()
 }
